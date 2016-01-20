8a1faabd07531a8b66c1a7a055e2b088340d38cc
mvr <matthewvonrocketstein@gmail-dot-com>
2016-01-20 23:41:43 +0000

    some fixes for no service.json found command

--- ymir/bin/_ymir.py
+++ ymir/bin/_ymir.py
@@ -1,31 +1,29 @@
+# -*- coding: utf-8 -*-
 """ ymir.bin._ymir
 """
 
-import os, sys
+import os
+import sys
 import logging
 from argparse import ArgumentParser
 
 from ymir.version import __version__
 from ymir.commands import (
     ymir_init, ymir_load, ymir_validate, ymir_sg,
-    ymir_eip, ymir_keypair)
+    ymir_eip, ymir_keypair, ymir_shell)
 
 from ymir.util import working_dir_is_ymir
 logger = logging.getLogger(__name__)
 
-LOG_LEVELS = [logging.CRITICAL, #50
-              logging.ERROR,    #40
-              logging.WARNING,  #30
-              logging.INFO,     #20
-              logging.DEBUG,    #10
+LOG_LEVELS = [logging.CRITICAL,  # 50
+              logging.ERROR,  # 40
+              logging.WARNING,  # 30
+              logging.INFO,  # 20
+              logging.DEBUG,  # 10
               ]
 
-def ymir_shell(args):
-    print 'not implemented yet'
 
 def ymir_freeze(args):
-    #name = args.name
-    #_id = args.instance_id
     print 'not implemented yet'
 
 
@@ -46,13 +44,14 @@ def get_parser():
     help_parser.set_defaults(subcommand='help')
     version_parser = subparsers.add_parser('version', help='show ymir version')
     version_parser.set_defaults(subcommand='version')
-    shell_parser = subparsers.add_parser('shell', help='open interactive shell')
+    shell_parser = subparsers.add_parser(
+        'shell', help='open interactive shell')
     shell_parser.set_defaults(subcommand='shell')
 
     eip_parser = subparsers.add_parser('eip', help='assign new elastic ip')
     eip_parser.set_defaults(subcommand='eip')
-    eip_parser.add_argument('-f','--force', action='store_true',
-                   help='force noninteractive mode')
+    eip_parser.add_argument('-f', '--force', action='store_true',
+                            help='force noninteractive mode')
 
     sgkargs = dict(metavar='security_group_json',
                    type=str,)
@@ -69,10 +68,10 @@ def get_parser():
         'security_group', help='updates AWS security group from JSON')
     security_group_parser.set_defaults(subcommand='security_group')
     security_group_parser.add_argument('sg_json', **sgkargs)
-    sg_parser.add_argument('-f','--force', action='store_true',
-                   help='force rules even if they dont support ssh')
-    security_group_parser.add_argument('-f','--force', action='store_true',
-                   help='force rules even if they dont support ssh')
+    sg_parser.add_argument('-f', '--force', action='store_true',
+                           help='force rules even if they dont support ssh')
+    security_group_parser.add_argument('-f', '--force', action='store_true',
+                                       help='force rules even if they dont support ssh')
 
     keypair_parser = subparsers.add_parser('keypair', help='show ymir keypair')
     keypair_parser.set_defaults(subcommand='keypair')
@@ -80,8 +79,8 @@ def get_parser():
         'keypair_name', metavar='keypair_name',
         type=str,
         help='a (new) keyname created on aws.  pem saved to ~/.ssh')
-    keypair_parser.add_argument('-f','--force', action='store_true',
-                   help='a (new) directory to initial a ymir project in')
+    keypair_parser.add_argument('-f', '--force', action='store_true',
+                                help='a (new) directory to initial a ymir project in')
 
     # build 'validate' subparser
     validate_parser = subparsers.add_parser(
@@ -107,11 +106,12 @@ def get_parser():
     load_parser.set_defaults(subcommand='load')
     init_parser = subparsers.add_parser('init', help='init ymir project')
     init_parser.add_argument('init_dir', metavar='directory', type=str,
-                   help='a (new) directory to initial a ymir project in')
-    init_parser.add_argument('-f','--force', action='store_true',
-                   help='force overwrite even if directory exists')
+                             help='a (new) directory to initial a ymir project in')
+    init_parser.add_argument('-f', '--force', action='store_true',
+                             help='force overwrite even if directory exists')
     init_parser.set_defaults(subcommand='init')
-    freeze_parser = subparsers.add_parser('freeze', help='freeze ymir service (must be running)')
+    freeze_parser = subparsers.add_parser(
+        'freeze', help='freeze ymir service (must be running)')
     freeze_parser.set_defaults(subcommand='freeze')
     return parser
 
@@ -130,34 +130,26 @@ def entry(settings=None):
 
     logging.basicConfig(
         level=level,
-        #format='%(levelname)s: %(message)s'
-        #format = '%(levelname)s [%(name)s:%(lineno)s] %(message)s'
         format="%(levelname)s [%(filename)s:%(lineno)s] %(message)s",
-        )
+    )
+    validate = lambda args: ymir_validate(args, simple=False)
+    subcommand_map = dict(
+        help=parser.print_help,
+        eip=ymir_eip,
+        security_group=ymir_sg,
+        init=ymir_init,
+        validate=validate,
+        load=ymir_load,
+        keypair=ymir_keypair,
+        freeze=ymir_freeze,
+        shell=ymir_shell,
+    )
     logger.debug('log level is: {0}'.format(level))
     if args.subcommand == 'sg':
         args.subcommand = 'security_group'
     if args.subcommand == 'version':
         print 'ymir=={0}'.format(__version__)
-    elif args.subcommand == 'help':
-        parser.print_help()
-    elif args.subcommand == 'eip':
-        ymir_eip(args)
-    elif args.subcommand == 'security_group':
-        ymir_sg(args)
-    elif args.subcommand == 'init':
-        ymir_init(args)
-    elif args.subcommand == 'validate':
-        ymir_validate(args, simple=False)
-    elif args.subcommand == 'load':
-        ymir_load(args)
-    elif args.subcommand == 'keypair':
-        ymir_keypair(args)
-    elif args.subcommand == 'freeze':
-        ymir_freeze(args)
-    elif args.subcommand == 'shell':
-        ymir_shell(args)
-    # reflect fabric here
-
-if __name__=='__main__':
+        return
+    subcommand_map[args.subcommand](args)
+if __name__ == '__main__':
     entry()
