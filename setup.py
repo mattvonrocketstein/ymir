dcb2178e3919a7b1b7d2d9d95debcc665971a905
mvr <matthewvonrocketstein@gmail-dot-com>
2016-02-04 15:59:38 +0000

    explicit requirements for boto/requests

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """ setup.py for ymir
 """
-import os, sys
+import os
+import sys
 from setuptools import setup
 
 # make sure that finding packages works, even
 # when setup.py is invoked from outside this dir
 this_dir = os.path.dirname(os.path.abspath(__file__))
-if not os.getcwd()==this_dir:
+if not os.getcwd() == this_dir:
     os.chdir(this_dir)
 
 # make sure we can import the version number so that it doesn't have
@@ -19,25 +21,29 @@ sys.path.pop()
 
 base_url = 'https://github.com/mattvonrocketstein/ymir/'
 setup(
-    name         = 'ymir',
-    version      = __version__,
-    description  = '',
-    author       = 'mattvonrocketstein',
-    author_email = '$author@gmail',
-    url          = base_url,
-    download_url = base_url+'/tarball/master',
-    packages     = ['ymir'],
-    keywords     = ['ymir'],
-    entry_points = {
-        'console_scripts': \
-        ['ymir = ymir.bin._ymir:entry', ] },
-    install_requires = [
+    name='ymir',
+    version=__version__,
+    description='',
+    author='mattvonrocketstein',
+    author_email='$author@gmail',
+    url=base_url,
+    download_url=base_url + '/tarball/master',
+    packages=['ymir'],
+    keywords=['ymir'],
+    entry_points={
+        'console_scripts':
+        ['ymir = ymir.bin._ymir:entry', ]},
+    install_requires=[
         'voluptuous',
         'demjson',
         'goulash',
-        'werkzeug', # used for caching
-        ],
-    #package_data={'ymir': ['skeleton/*']},
-    include_package_data=True, # this will use MANIFEST.in during install where we specify additional files
+        'werkzeug',  # used for caching
+        'boto',
+        'requests',
+    ],
+    # package_data={'ymir': ['skeleton/*']},
+    # this will use MANIFEST.in during install where we specify additional
+    # files
+    include_package_data=True,
 
-    )
+)
