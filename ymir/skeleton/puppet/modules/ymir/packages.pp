b0d2edd9b5e908ea143c9f501186dc784cd4986c
mvr <matthewvonrocketstein@gmail-dot-com>
2016-05-05 23:04:53 +0000

    add mosh to suggested packages in skeleton

--- ymir/skeleton/puppet/modules/ymir/packages.pp
+++ ymir/skeleton/puppet/modules/ymir/packages.pp
@@ -6,10 +6,10 @@
 #
 package { 'nmap': ensure => present }
 package { 'htop': ensure => present }
+package { 'mosh': ensure => present }
 package {'tree': ensure=> present }
 package {'ack-grep': ensure=> present }
 package {'mosh': ensure=> present }
 package {'python-dev': ensure=> present }
 package {'python-virtualenv': ensure=> present }
 package {['ruby-dev', 'gem']: ensure => present }
-
