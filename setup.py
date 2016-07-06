aa07b8952d16587211b2141748fd0e73e67d8d40
mvr <matthewvonrocketstein@gmail-dot-com>
2016-07-06 23:49:25 +0000

    move all requirements into setup, the better to pypi with

--- setup.py
+++ setup.py
@@ -5,13 +5,6 @@
 import os
 import sys
 from setuptools import setup
-try:
-    from pip.req import parse_requirements  # flake8: noqa
-except ImportError:
-    raise SystemExit("please install pip before installing ymir")
-install_reqs = parse_requirements(os.path.join(
-    os.path.dirname(__file__), 'requirements.txt'), session=False)
-reqs = [str(ir.req) for ir in install_reqs]
 
 # make sure that finding packages works, even
 # when setup.py is invoked from outside this dir
@@ -23,7 +16,7 @@ if not os.getcwd() == this_dir:
 # to be changed in two places. ymir/__init__.py is also free
 # to import various requirements that haven't been installed yet
 sys.path.append(os.path.join(this_dir, 'ymir'))
-from version import __version__
+from version import __version__  # flake8: noqa
 sys.path.pop()
 
 base_url = 'https://github.com/mattvonrocketstein/ymir/'
@@ -40,8 +33,25 @@ setup(
     entry_points={
         'console_scripts':
         ['ymir = ymir.bin._ymir:entry', ]},
-    install_requires=reqs,
-
+    # install_requires=reqs,
+    install_requires=[
+        "ansible==2.1.0.0",
+        "awsebcli==3.1.2",
+        "boto==2.36.0",
+        "demjson==2.2.4",
+        "Fabric==1.10.1",
+        "Importing==1.10",
+        "Jinja2==2.8",
+        "pycrypto==2.6.1",
+        "python-vagrant==0.5.13",
+        "requests==2.5.1",
+        "retrying==1.3.3",
+        "testinfra==1.3.0",
+        "voluptuous==0.8.11",
+        "Werkzeug==0.11.10",      # used for caching
+        ""
+        "YURL==0.13",
+    ],
     # package_data={'ymir': ['skeleton/*']},
     # this will use MANIFEST.in during install where we specify additional
     # files
