b1861c367bbacf979e7236b546e0f4182b54d159
mvr <matthewvonrocketstein@gmail-dot-com>
2016-07-06 22:54:05 +0000

    use api.task decorator to publish all custom fabric commands

--- ymir/skeleton/fabfile.py
+++ ymir/skeleton/fabfile.py
@@ -22,12 +22,14 @@ _service = load_service_from_json(YMIR_SERVICE_JSON)
 _service.fabric_install()
 
 
+@api.task
 def deploy(branch='master'):
     """ example usage: "fab deploy:branch=master" """
     _service.report("deploy for branch {0} -> {1} is not defined yet".format(
         branch, _service))
 
 
+@api.task
 def tail_syslog():
     """ example: tail syslog on remote server """
     with _service.ssh_ctx():
