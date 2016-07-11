06f4984a72e43198d3072a9be262a2dcc7ea44f9
mvr <matthewvonrocketstein@gmail-dot-com>
2016-07-11 14:01:45 +0000

    adding hiera stuff to skeleton

--- ymir/util/puppet.py
+++ ymir/util/puppet.py
@@ -11,7 +11,8 @@ DEFAULT_FACTS = [
 ]
 
 
-def run_puppet(_fname, parser=None, debug=False, puppet_dir=None, facts={}):
+def run_puppet(_fname, parser=None, debug=False,
+               hiera_config=None, puppet_dir=None, facts={}):
     """ NB: must be run within a fabric ssh context """
     _facts = {}
     for fact_name, val in facts.items():
@@ -24,8 +25,16 @@ def run_puppet(_fname, parser=None, debug=False, puppet_dir=None, facts={}):
     with api.shell_env(**_facts):
         # sudo -E preserves the invoking enviroment,
         # thus we are able to pass through the facts
-        api.run("sudo -E `which puppet` apply {parser} {debug} --modulepath={pdir}/modules {fname}".format(
-            parser='', #('--parser ' + parser) if parser else '',
-            debug='--debug' if debug else '',
-            pdir=puppet_dir or os.path.dirname(_fname),
-            fname=_fname))
+        puppet_cmd = ("sudo -E `which puppet` "
+                      "apply {parser} {debug} "
+                      "--hiera_config {hiera_config}"
+                      "--modulepath={pdir}/modules {fname}")
+        pdir = puppet_dir or os.path.dirname(_fname)
+        hconfig = hiera_config or '{pdir}/hiera.yaml'.format(pdir)
+        api.run(
+            puppet_cmd.format(
+                parser='',  # ('--parser ' + parser) if parser else '',
+                debug='--debug' if debug else '',
+                pdir=pdir,
+                fname=_fname,
+                hiera_config=hconfig))
